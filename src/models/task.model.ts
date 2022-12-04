602a55769981ec96767962c69ccf9b00433d465d
TuiTenTien <phungthanhtien01@gmail.com>
2022-12-04 16:29:15 +0700

    add Task model

---
+++ src/models/task.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property} from '@loopback/repository';
+import { ETaskStatus } from '../enum';
+
+@model()
+export class Task extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  isCreatedByAdmin: boolean;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'date',
+  })
+  createdAt?: string;
+
+  @property({
+    type: 'date',
+  })
+  updatedAt?: string;
+
+  @property.array(ETaskStatus,{
+    default: ETaskStatus.TODO
+  })
+  status?: ETaskStatus[];
+
+
+  constructor(data?: Partial<Task>) {
+    super(data);
+  }
+}
+
+export interface TaskRelations {
+  // describe navigational properties here
+}
+
+export type TaskWithRelations = Task & TaskRelations;
